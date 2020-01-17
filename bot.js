a8b3c8a15f94354a790afed34d9e32f30f5400d6
saifdz1 <40006650+saifdz1@users.noreply.github.com>
2020-01-17 17:36:36 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,12 +42,39 @@ if(message.content.split(' ')[0] == '-bc') {
         message.guild.members.forEach(m => {
             var bc = new Discord.RichEmbed()
 .addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
-            .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
+            .addField('***● Server  :***', `** → ${message.guild.name}**`)               
     .setColor('#ff0000')
                  .addField('ّ', args)
             m.send(``,{embed: bc});
         });
     }
 })
+
+client.on('message' , message => {
+      var prefix = "-";
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(prefix + "bcrole")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | `$rolebc role message`")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(`${codes}`)
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
 
 client.login(process.env.BOT_TOKEN);
